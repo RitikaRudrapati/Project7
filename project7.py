a78684e155fbafc836a2b9d7403264189b5d1168
Ritika Rudrapati <riitkarudrapati@gmail.com>
2026-02-04 18:42:49 -0600

    created pop and push local function

--- project7.py
+++ project7.py
@@ -21,13 +21,30 @@ def pushLocal(value):
     return message
 
 def popLocal(value):
-    message  = decrementSP() #points sp to the top value 
-    message += "A=M\n" #D = RAM[SP]
-    message += "D=M\n" #D has the value we want to store in local[i]
-
+    #need to find the address to pop into (MAKE SURE TO STORE IN TEMP REGISTER)
+    message += "@" + str(value) + "\n" 
+    message += "D=A\n"             # D = value 
     message += "@LCL\n"
+    message += "A=M\n"             #A points to the base address of the LCL segment. 
+    message += "D=D+A\n"           # now let d be the target address of LCL+value
+    message += "@R13\n"
+    message += "M=D\n" 
+
+    #pop the top value off the stack
+    message  = decrementSP()       # SP-- 
+    message += "A=M\n"             #A points to the actual memory location of the top value
+    message += "D=M\n"             # D = get top of the stack value 
+
+    message += "@R13\n"
+    message += "A=M"
+    message += "M=D"
 
     
+
+    message += incrementSP()       # increment SP back
+
+    
+
     return message
 
 #--------------------------HELPER FUNCTIONS--------------------------#
@@ -35,11 +52,15 @@ def pushValue():
     message += "@SP\n"
     message += "A=M\n"
     message += "M=D\n"
-    message += "@SP\n" 
-    message += "M=M+1\n"
+    message += incrementSP()
     return message
 
 def decrementSP():
     message  = "@SP\n"
     message += "M=M-1\n"
+    return message
+
+def incrementSP():
+    message  = "@SP\n"
+    message += "M=M+1\n"
     return message
