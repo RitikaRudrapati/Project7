f793bd63c75424ad0dd10a38cd8c3813fdfa3374
Ritika Rudrapati <riitkarudrapati@gmail.com>
2026-02-08 23:42:56 -0600

    final project submission - debugged the feature of where I'm writing to a file,

---
+++ README.md
@@ -0,0 +1,62 @@
+This project is a Python-based translator that converts Hack Virtual Machine (VM) code into Hack Assembly code (.asm).
+
+
+
+It allows for basic operations like push/pop, arithmetic commands, and logical operations.
+
+This program is specific to Hack Assembly
+
+
+
+Features:
+
+
+
+Push/Pop:
+
+
+1- constant
+2- local
+3- argument
+4- this
+5- that
+6- temp
+7- pointer
+8- static
+
+
+
+Arithmetic Operations:
+
+
+
+1- add
+2- sub
+3- and
+4- or
+
+
+
+Logical:
+
+
+
+1- eq
+2- gt
+3- lt
+
+
+
+
+
+How to Use it:
+
+create a .vm file
+edit the readFile() call in project7.py with the correctfile path
+run the python script
+the translated assembly code will be written to an .asm file
+
+
+
+*Test file is included*
+
--- project7.py
+++ project7.py
@@ -1,3 +1,7 @@
+import os
+
+global message
+
 #--------------------------constant push and pop--------------------------#
 count = 0
 
@@ -7,32 +11,27 @@ def pushConstant(value):
     message += "D=A\n"
     pushValue()
 
-
 #--------------------------SEGMENT POP AND PUSH--------------------------#
-
 #accepted parameter for segement: @LCL, @ARG, @THIS, @THAT, 
 def pushSegment(segment, value):
     global message
     message  += "@" + str(value) + "\n"
     message += "D=A\n"
-    message += "@" + str(segment) + "\n"
+    message += str(segment) + "\n"
     message += "A=M+D\n"
     message += "D=M\n"
     pushValue()
 
-#accepted parameter for segement: @LCL, @ARG, @THIS, @THAT, 
 def popSegment(segment, value):
     global message
-    #need to find the address to pop into (MAKE SURE TO STORE IN TEMP REGISTER)
     message += "@" + str(value) + "\n" 
     message += "D=A\n"             # D = value 
-    message += "@" + str(segment) + "\n"
+    message += str(segment) + "\n"
     message += "A=M\n"             #A points to the base address of the segment. 
     message += "D=D+A\n"           # now let d be the target address of LCL+value
     message += "@R13\n"
     message += "M=D\n" 
 
-    #pop the top value off the stack
     decrementSP()       # SP is getting ready to pop the top value off the stack 
     message += "A=M\n"             #A points to the actual memory location of the top value
     message += "D=M\n"             # D = get top of the stack value 
@@ -42,8 +41,6 @@ def popSegment(segment, value):
     message += "M=D\n"
     
 #--------------------------temp push and pop--------------------------#
-
-#temp segment is fixed at 5-12, so we can just add the value to 5 to get the correct address.
 def pushTemp(value):
     global message
     message  += "@" + str(5 + value) + "\n"
@@ -52,7 +49,6 @@ def pushTemp(value):
 
 def popTemp(value):
     global message
-
     decrementSP()
     message += "A=M\n"
     message += "D=M\n"
@@ -60,18 +56,14 @@ def popTemp(value):
     message += "M=D\n"
 
 #--------------------------pointer push and pop--------------------------#
-
-#pointer segment is fixed at 3-4, so we can just add the value to 3 to get the correct address.
 def pushPointer(value):
     global message
-
     message  += "@" + str(3 + value) + "\n"
     message += "D=M\n"
     pushValue()
 
 def popPointer(value):
     global message
-
     decrementSP()
     message += "A=M\n"
     message += "D=M\n"
@@ -79,54 +71,40 @@ def popPointer(value):
     message += "M=D\n"
 
 #--------------------------Arithmetic commands--------------------------#
-
-#acceptable parameters: +, -, &, | 
 def arithmetic(command):
     global message
-
     decrementSP() 
     message += "D=M\n" # y
-
     decrementSP() 
     message += "A=M\n" 
     message += "D=D" + command + "M\n" # x (command) y
-
     pushValue() 
 
-
 #--------------------------static pop and push--------------------------#
-
-# The name of the variable is file.value, where file is the name of the current file.
 def pushStatic(value):
     global message
-
     message  += "@" + file + "." + str(value) + "\n"
     message += "D=M\n"
     pushValue()
 
 def popStatic(value):
     global message
-
     decrementSP()
     message += "A=M\n"
     message += "D=M\n"
     message += "@" + file + "." + str(value) + "\n"
     message += "M=D\n"
 
-
 #--------------------------logical commands--------------------------#
 def logical(command):
     global message, count
-
     count += 1 # need to make sure that the labels are unique for each logical command
-
     true = f"EQ_{count}"
     false = f"EQ_f{count}"
 
     decrementSP()
     message += "A=M\n"
     message += "D=M\n"   # Store the top value in D (y)
-
 
     decrementSP()
     message += "A=M\n"
@@ -153,90 +131,6 @@ def logical(command):
 
     pushValue()
 
-
-#--------------------------reading in the files--------------------------#
-
-def readFile(fileName):
-    global message
-    message = ""
-    global file
-    file = fileName.split("/")[-1].split(".")[0]
-    with open(fileName) as f:
-        lines = f.readlines()
-    for line in lines: 
-        # remove comments and whitespace
-            line = line.split("//")[0].strip()
-            if line == "":
-                continue
-
-            parts = line.split()
-
-            if parts[0] == "push":
-                segment = parts[1]
-                value = int(parts[2])
-
-                if segment == "constant":
-                    pushConstant(value)
-                elif segment == "local":
-                    pushSegment("@LCL", value)
-                elif segment == "argument":
-                    pushSegment("@ARG", value)
-                elif segment == "this":
-                    pushSegment("@THIS", value)
-                elif segment == "that":
-                    pushSegment("@THAT", value)
-                elif segment == "temp":
-                    pushTemp(value)
-                elif segment == "pointer":
-                    pushPointer(value)
-                elif segment == "static":
-                    pushStatic(value)
-
-            elif parts[0] == "pop":
-                segment = parts[1]
-                value = int(parts[2])
-
-                if segment == "local":
-                    popSegment("@LCL", value)
-                elif segment == "argument":
-                    popSegment("@ARG", value)
-                elif segment == "this":
-                    popSegment("@THIS", value)
-                elif segment == "that":
-                    popSegment("@THAT", value)
-                elif segment == "temp":
-                    popTemp(value)
-                elif segment == "pointer":
-                    popPointer(value)
-                elif segment == "static":
-                    popStatic(value)
-            
-            elif parts[0] in ["add", "sub", "and", "or"]:
-                if parts[0] == "add":
-                    parts[0] = "+"
-                elif parts[0] == "sub":
-                    parts[0] = "-"
-                elif parts[0] == "and":
-                    parts[0] = "&"
-                elif parts[0] == "or":
-                    parts[0] = "|"
-                else:
-                    message += "Incorrect command: " + parts[0]    
-                arithmetic(parts[0])
-
-            elif parts[0] in ["eq", "gt", "lt"]:
-                logical(parts[0])
-
-            else:
-                message += "Incorrect command: " + parts[0]
-    
-    return lines
-
-def writeFile(outputFile, message):
-    with open(outputFile, "w") as f:
-        f.write(message)
-
-
 #--------------------------HELPER FUNCTIONS--------------------------#
 def pushValue():
     global message
@@ -254,3 +148,88 @@ def incrementSP():
     global message
     message += "@SP\n"
     message += "M=M+1\n"
+
+#--------------------------reading in the files--------------------------#
+def readFile(fileName):
+    global message
+    message = ""
+    global file
+    file = os.path.basename(fileName).split(".")[0]
+    with open(fileName) as f:
+        lines = f.readlines()
+    for line in lines: 
+        line = line.split("//")[0].strip()
+        if line == "":
+            continue
+        parts = line.split()
+        if parts[0] == "push":
+            segment = parts[1]
+            value = int(parts[2])
+            if segment == "constant":
+                pushConstant(value)
+            elif segment == "local":
+                pushSegment("@LCL", value)
+            elif segment == "argument":
+                pushSegment("@ARG", value)
+            elif segment == "this":
+                pushSegment("@THIS", value)
+            elif segment == "that":
+                pushSegment("@THAT", value)
+            elif segment == "temp":
+                pushTemp(value)
+            elif segment == "pointer":
+                pushPointer(value)
+            elif segment == "static":
+                pushStatic(value)
+        elif parts[0] == "pop":
+            segment = parts[1]
+            value = int(parts[2])
+            if segment == "local":
+                popSegment("@LCL", value)
+            elif segment == "argument":
+                popSegment("@ARG", value)
+            elif segment == "this":
+                popSegment("@THIS", value)
+            elif segment == "that":
+                popSegment("@THAT", value)
+            elif segment == "temp":
+                popTemp(value)
+            elif segment == "pointer":
+                popPointer(value)
+            elif segment == "static":
+                popStatic(value)
+        elif parts[0] in ["add", "sub", "and", "or"]:
+            if parts[0] == "add":
+                parts[0] = "+"
+            elif parts[0] == "sub":
+                parts[0] = "-"
+            elif parts[0] == "and":
+                parts[0] = "&"
+            elif parts[0] == "or":
+                parts[0] = "|"
+            arithmetic(parts[0])
+        elif parts[0] in ["eq", "gt", "lt"]:
+            logical(parts[0])
+        else:
+            message += "Incorrect command: " + parts[0]
+
+def writeToFile():
+    global message
+
+    #code credit to stack overflow for this part: https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
+    folder = os.path.dirname(test_path)
+    base_name = os.path.basename(test_path).split(".")[0]
+    output_path = os.path.join(folder, base_name + ".asm")    
+
+    with open(output_path, "w") as outfile:
+        outfile.write(message)
+
+
+
+#--------------------------RUN THE CODE--------------------------#
+
+#hardcode this for testing purposes, can change it to read in a directory of files later 
+test_path = r"C:\Users\ritik\ECS_2\project7\test.vm"
+
+readFile(test_path)
+writeToFile()
